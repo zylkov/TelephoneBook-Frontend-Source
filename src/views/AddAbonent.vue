68c0b2f427dd8032e6658d9261b5d9be69d97520
Pixel-Tentacles <zylkov2010@gmail.com>
2019-01-31 00:37:12 +0300

    just input

---
+++ src/views/AddAbonent.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="AddAbonent">
+        <v-layout align-center justify-center>
+            <v-card class="list">
+                <v-list two-line>
+                    <v-list-tile class="item-list">
+                        <v-list-tile-content>
+                            <v-list-tile-title>Фамилия</v-list-tile-title>
+                            <v-list-tile-sub-title>
+                                <v-text-field
+                                placeholder="Зыльков"
+                                required/>
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider/>
+                    <v-list-tile class="item-list">
+                        <v-list-tile-content>
+                            <v-list-tile-title>Имя</v-list-tile-title>
+                            <v-list-tile-sub-title>
+                                <v-text-field
+                                placeholder="Даниил"
+                                required/>
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider/>
+                    <v-list-tile class="item-list">
+                        <v-list-tile-content>
+                            <v-list-tile-title>Отчество</v-list-tile-title>
+                            <v-list-tile-sub-title>
+                                <v-text-field
+                                placeholder="Алексеевич"
+                                required/>
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider/>
+                    <v-list-tile class="item-list">
+                        <v-list-tile-content>
+                            <v-list-tile-title>Место прописки</v-list-tile-title>
+                            <v-list-tile-sub-title>
+                                <v-text-field
+                                placeholder="Рощино"
+                                required
+                                />
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider/>
+                    <v-list-tile class="item-list">
+                        <v-list-tile-content>
+                            <v-list-tile-title>Адресс</v-list-tile-title>
+                            <v-list-tile-sub-title>
+                                <v-text-field
+                                placeholder="Проезд Строителей; дом 123; кв 21"
+                                />
+                                
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider/>
+                    <v-list-tile class="item-list">
+                        <v-list-tile-content>
+                            <v-list-tile-title>Номер телефона</v-list-tile-title>
+                            <v-list-tile-sub-title>
+                                <v-text-field required/>
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-card>
+       </v-layout>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"AddAbonent"
+}
+</script>
+
+<style>
+.item-list{
+    padding-top:10px;
+    padding-bottom: 10px; 
+}
+</style>
