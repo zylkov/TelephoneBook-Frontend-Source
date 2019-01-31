688eec92db3908a630128c40397ea81e67fabc95
Pixel-Tentacles <zylkov2010@gmail.com>
2019-01-31 23:48:38 +0300

    change size in nav

--- src/components/layout/Navigator.vue
+++ src/components/layout/Navigator.vue
@@ -15,11 +15,11 @@
             @click="goToPage(item.pathName)"
           >
             <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
+              <v-icon medium>{{ item.icon }}</v-icon>
             </v-list-tile-action>
   
             <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-title class="title font-weight-light">{{ item.title }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
