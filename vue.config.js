cddf1908e73eb30fb2725e3f5be4e017085fb452
zylkov <zylkov.work@gmail.com>
2019-08-19 18:14:35 +0300

    change path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    baseUrl: process.env.VUE_APP_MODE === 'production' ? '/TelephoneBook-Frontend/' : '/',
+  publicPath: process.env.VUE_APP_MODE === 'production' ? '/TelephoneBook-Frontend/' : '/',
   }
